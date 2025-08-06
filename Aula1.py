7fb673e335e847acfb9e5af342e2833f14199306
Andrellz <andrellzcaetano@ufu.br>
2025-08-05 22:40:40 -0300

    Semana 2 aula 2

--- Aula1.py
+++ Aula1.py
@@ -1,1 +1,37 @@
-print('my first program')
+"""Semana 2 - aula 2"""
+
+# Palavras reservadas
+# True, False, None, and, import, lambda
+
+# Identificadores
+# nome de variáveis, funções, classes
+# case sensitive
+nome = 'Maria'
+idade = 30
+Nome = 'João'
+nome_completo = 'Maria da Silva'
+# nao pode ter espaços em branco
+# nao pode usar caracteres especiais !, @, #, $
+
+# Boas práticas (clean code)
+c = 10
+contador = 10
+
+s = 10 + 20
+soma = 10 + 20
+
+# Constantes
+PI = 3.14
+IDADE_MINIMA = 18
+
+idade = 18
+if idade >= IDADE_MINIMA:
+    print('Maior de idade')
+else:
+    print('Menor de idade')
+
+
+
+
+
+
