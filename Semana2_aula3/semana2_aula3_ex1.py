ae421e20a694324a61ae252991b48e786bdb28c1
Andrellz <andrellzcaetano@ufu.br>
2025-08-06 10:56:31 -0300

    pasta da aula3

---
+++ Semana2_aula3/semana2_aula3_ex1.py
@@ -0,0 +1,10 @@
+'''Tipos de dados'''
+
+x = int(input("Digita o primeiro numero:"))
+y = int(input("Digita o segundo numero:"))
+w = int(input("Digita o terceiro numero:"))
+
+lista = [x , y, w]
+
+print(f"o maior valor digitado foi {max(lista)} e o menor foi {min(lista)}")
+
